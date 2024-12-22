f5264a330fd8e7caf3b1f4734fc34cff050b1810
Nikola Zivkovic <emptyclip.srb@gmail.com>
2024-12-22 22:06:48 +0100

    using adminlte template for login + added menu for the dashboard.

--- config/adminlte.php
+++ config/adminlte.php
@@ -30,9 +30,9 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
+    'logo' => '<b>Data Import</b>',
 
-    'logo_mini' => '<b>A</b>LT',
+    'logo_mini' => '<b>D</b>I',
 
     /*
     |--------------------------------------------------------------------------
@@ -127,83 +127,36 @@ return [
 
     'menu' => [
         [
-            'text' => 'search',
-            'search' => true,
-        ],
-        ['header' => 'main_navigation'],
-        [
-            'text' => 'blog',
-            'url'  => 'admin/blog',
-            'can'  => 'manage-blog',
-        ],
-        [
-            'text'        => 'pages',
-            'url'         => 'admin/pages',
-            'icon'        => 'far fa-file',
-            'label'       => 4,
-            'label_color' => 'success',
-        ],
-        ['header' => 'account_settings'],
-        [
-            'text' => 'profile',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-user',
-        ],
-        [
-            'text' => 'change_password',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-lock',
-        ],
-        [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
+            'text' => 'User Management',
+            'icon' => 'fas fa-fw fa-users',
+            'can'  => 'user-management',
             'submenu' => [
                 [
-                    'text' => 'level_one',
-                    'url'  => '#',
+                    'text' => 'Users',
+                    'url'  => '/users',
+                    'icon' => 'fas fa-fw fa-user',
                 ],
                 [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
+                    'text' => 'Permissions',
+                    'url'  => '/permissions',
+                    'icon' => 'fas fa-fw fa-cog',
                 ],
             ],
         ],
-        ['header' => 'labels'],
         [
-            'text'       => 'important',
-            'icon_color' => 'red',
+            'text' => 'Data Import',
+            'url'  => 'admin/pages',
+            'icon' => 'fas fa-fw fa-cubes',
         ],
         [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
+            'text' => 'Imported Data',
+            'url'  => 'admin/settings',
+            'icon' => 'fas fa-fw fa-line-chart',
         ],
         [
-            'text'       => 'information',
-            'icon_color' => 'aqua',
+            'text' => 'Imports',
+            'url'  => 'admin/settings',
+            'icon' => 'fas fa-fw fa-cubes',
         ],
     ],
 
